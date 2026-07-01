20558678b8a0781600214abed378ac43698b7f06
Hunar Kaur <126564461+HunarKaur2007@users.noreply.github.com>
2026-07-02 01:31:46 +0530

    Time: 4 ms (87.52%), Space: 22.8 MB (98.48%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -3,15 +3,15 @@ public:
     bool caneat(vector<int>&piles, int h, int speed){
         long long hours =0;
         for(int p:piles){
-            hours+=(p+speed-1)/speed;
+            hours+=(p+speed-1)/speed; //ceil val of p/speed
             if(hours>h) return false;
         }
         return true;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low=1;
+        int low=1; //koko has to eat atleast 1 banana per hour
         int high= *max_element(piles.begin(),piles.end());
-
+//* gives actual value
         while(low<=high){
             int mid =low+(high-low)/2;
             if(caneat(piles,h,mid)) high =mid -1;
