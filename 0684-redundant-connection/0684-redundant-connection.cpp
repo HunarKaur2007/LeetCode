7f7d3ef34dec5116e4bdb089e4d638eff60fe8dc
Hunar Kaur <126564461+HunarKaur2007@users.noreply.github.com>
2026-08-22 18:13:02 +0530

    Time: 0 ms (100%), Space: 13.4 MB (37.23%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -10,16 +10,17 @@ public:
         parent.resize(n+1);
         for(int i=1;i<=n;i++){
             parent[i]=i;
-        }
+        }//make every node its own parent
         for(auto edge : edges){
+            //extract 2 nodes
             int u= edge[0];
             int v= edge[1];
-
+            //find thier roots
             int pu=find(u);
             int pv=find(v);
-
-            if(pu==pv) return edge;
-
+            //if pu = pv that means u and v are already connected, adding this edge would create a cycle thus its redundant edge
+            if(pu==pv) return edge; //same grp = cycle
+            //merge 2 grps if grps different
             parent[pu] = pv;
         }
         return {};
