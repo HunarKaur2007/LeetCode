df5a3e65eb8ee158061fdb7e116bf48615a3803a
Hunar Kaur <126564461+HunarKaur2007@users.noreply.github.com>
2026-08-27 13:12:15 +0530

    Time: 0 ms (100%), Space: 16.9 MB (45.15%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,75 +1,66 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
 
-        int rows = grid.size();
-        int cols = grid[0].size();
+        queue<pair<pair<int,int>, int>> q;
 
-        queue<pair<int,int>> q;
-        int fresh = 0;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
 
-        // Find all rotten oranges and count fresh ones
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-
-                if(grid[i][j] == 2)
-                    q.push({i, j});
-
-                else if(grid[i][j] == 1)
-                    fresh++;
-            }
-        }
-
-        // No fresh oranges
-        if(fresh == 0)
-            return 0;
-
-        int minutes = 0;
-
-        // 4 directions: up, down, left, right
-        int dr[] = {-1, 1, 0, 0};
-        int dc[] = {0, 0, -1, 1};
-
-        while(!q.empty()) {
-
-            int size = q.size();
-            bool rottenThisMinute = false;
-
-            // Process one level (one minute)
-            while(size--) {
-
-                auto curr = q.front();
-                q.pop();
-
-                int r = curr.first;
-                int c = curr.second;
-
-                // Check all 4 neighbours
-                for(int k = 0; k < 4; k++) {
-
-                    int nr = r + dr[k];
-                    int nc = c + dc[k];
-
-                    // Valid fresh orange
-                    if(nr >= 0 && nr < rows &&
-                       nc >= 0 && nc < cols &&
-                       grid[nr][nc] == 1) {
-
-                        grid[nr][nc] = 2;      // Rot it
-                        fresh--;               // One less fresh orange
-                        q.push({nr, nc});      // It will spread next minute
-                        rottenThisMinute = true;
-                    }
+        // Put all initially rotten oranges in queue
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                
+                if(grid[i][j] == 2) {
+                    q.push({{i, j}, 0});
+                    vis[i][j] = 1;
                 }
             }
-
-            if(rottenThisMinute)
-                minutes++;
         }
 
-        if(fresh > 0)
-            return -1;
+        int tm = 0;
 
-        return minutes;
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
+
+        while(!q.empty()) {
+            
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int t = q.front().second;
+
+            q.pop();
+
+            tm = max(tm, t);
+
+            for(int i = 0; i < 4; i++) {
+                
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
+
+                if(nrow >= 0 && nrow < n &&
+                   ncol >= 0 && ncol < m &&
+                   grid[nrow][ncol] == 1 &&
+                   vis[nrow][ncol] == 0) {
+                    
+                    q.push({{nrow, ncol}, t + 1});
+                    vis[nrow][ncol] = 1;
+                }
+            }
+        }
+
+        // Check if any fresh orange is left
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                
+                if(grid[i][j] == 1 && vis[i][j] == 0) {
+                    return -1;
+                }
+            }
+        }
+
+        return tm;
     }
 };
