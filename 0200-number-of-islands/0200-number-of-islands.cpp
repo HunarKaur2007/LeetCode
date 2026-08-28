bb93fb960221fdf6a9a9136f1425af09658a418e
Hunar Kaur <126564461+HunarKaur2007@users.noreply.github.com>
2026-08-28 11:40:23 +0530

    Time: 25 ms (71.77%), Space: 17.3 MB (51.15%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    void dfs(int row, int col,vector<vector<char>>&grid, vector<vector<int>>&vis, vector<pair<int,int>> &vec){
+    void dfs(int row, int col,vector<vector<char>>&grid, vector<vector<int>>&vis){
 
         int n = grid.size();
         int m = grid[0].size();
 
         vis[row][col] =1;
-        vec.push_back({row,col});
+     //   vec.push_back({row,col});
 
         int dr[] = {-1,0,1,0};
         int dc[] = {0,1,0,-1};
@@ -16,7 +16,7 @@ public:
             int nc = col + dc[i];
 
             if(nr >= 0 && nr < n && nc >=0 && nc < m && grid[nr][nc] == '1' && vis[nr][nc] == 0){
-                dfs(nr,nc,grid,vis,vec);
+                dfs(nr,nc,grid,vis);
             }
         }
     }
@@ -26,17 +26,20 @@ public:
         int m = grid[0].size();
 
         vector<vector<int>> vis(n,vector<int>(m,0));
-        set<vector<pair<int,int>>>st;
+       // set<vector<pair<int,int>>>st;
+        int count =0;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(!vis[i][j] && grid[i][j]=='1'){
-                    vector<pair<int,int>> vec;
-                    dfs(i,j,grid,vis,vec);
-                    st.insert(vec);
+                    count++;
+                    //vector<pair<int,int>> vec;
+                    dfs(i,j,grid,vis);
+
+                    //st.insert(vec);
                 }
             }
         }
-        return st.size();
+        return count ;
     }
 };
